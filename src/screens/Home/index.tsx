c853b9fb32e09380e743653d640f82125d8c9d47
Paulo Filho <pcf.feat@gmail.com>
2021-11-19 12:34:38 -0400

    chore: basic scaffolding

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>Hey there</Text>
+    </Container>
+  );
+}
