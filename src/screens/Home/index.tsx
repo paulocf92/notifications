6893b0f22486d07ae2dfee2179bec0243866ac19
Paulo Filho <pcf.feat@gmail.com>
2021-11-19 14:01:59 -0400

    feat: add firebase cloud messaging test

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 import { Container } from './styles';
 
 export default function Home() {
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log({ token });
+  };
+
+  useEffect(() => {
+    getToken();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', {
+        message: JSON.stringify(remoteMessage),
+      });
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('A new BACKGROUND FCM message arrived!', {
+        message: JSON.stringify(remoteMessage),
+      });
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Container>
       <Text>Hey there</Text>
